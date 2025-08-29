575787b8103d2252ad93c999bfdb76d037484a89
Joharie Kisiangani <joharie.takataka@gmail.com>
2025-08-29 01:53:08 +0000

    scan:1  Failed to load resource: the server responded with a status of 5

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  serverActions: {
+    bodySizeLimit: '5mb',
+  },
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
